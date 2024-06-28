e9046f51e7eda389662bd7e9e6330d188d7693d9
StraponBace <ponomarev.tima2005@mail.ru>
2024-06-28 19:22:26 +0700

    set dbname and чпу

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=cleaning',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
